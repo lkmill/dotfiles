a11918141b6b5519e0787013a5b34d5fc0527993
Linus Miller <lohfu@lohfu.io>
2018-12-09 21:26:45 +0100

    qutebrowser: initial commit

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,19 @@
+c.url.searchengines = {
+    'DEFAULT': 'https://duckduckgo.com/?q={}',
+    'af': 'https://duckduckgo.com/?q=site%3Ahttps%3A%2F%2Fbbs.archlinux.org+{}&ia=web',
+    'aw': 'https://wiki.archlinux.org/index.php?title=Special%3ASearch&search={}&go=Go',
+    'gh': 'https://duckduckgo.com/?q=site%3Ahttps%3A%2F%2Fgithub.com+{}',
+    'gi': 'https://www.google.se/search?source=lnms&tbm=isch&q={}',
+    'im': 'http://iconmonstr.com/?s={}',
+    'mdn': 'https://duckduckgo.com/?q=site%3Ahttps%3A%2F%2Fdeveloper.mozilla.org%2Fen-US%2F+{}',
+    't': 'http://www.thesaurus.com/browse/{}',
+    'w': 'http://wikipedia.org/w/index.php?search={}',
+    'ws': 'http://sv.wikipedia.org/w/index.php?search={}',
+    'yt': 'https://www.youtube.com/results?search_query={}&page={{startPage?}}',
+}
+
+c.tabs.background = True
+c.bindings.key_mappings['<Ctrl+i>'] = '<Tab>'
+c.bindings.key_mappings['<Ctrl+h>'] = '<Backspace>'
+c.bindings.key_mappings['<Ctrl+j>'] = '<Down>'
+c.bindings.key_mappings['<Ctrl+k>'] = '<Up>'
