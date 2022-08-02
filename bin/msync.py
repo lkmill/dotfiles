767c9b99c089880594c7c65baf8c4d552c59d59d
Linus Miller <linus.miller@bitmill.io>
2022-08-02 16:33:07 +0200

    bin: handle symlinks in msync.py

--- bin/msync.py
+++ bin/msync.py
@@ -10,6 +10,8 @@ home_root = '~'
 home_dirs = [
   'archives',
   'photos',
+  'tmp',
+  'tmp2',
 ]
 
 storage_dirs = [
@@ -24,10 +26,9 @@ group.add_argument('-B', '--backup', help='Backup files to the remote location',
 group.add_argument('-R', '--restore', help='Restore files from the remote location', action='store_true')
 parser.add_argument('-d', '--dry-run', help='Run rsync in a dry run', action='store_true')
 parser.add_argument('-r', '--remote', help='The remote location', default='lohfu@loader')
-parser.add_argument('-l', '--local', help='The local location', default='/mnt/storage')
+parser.add_argument('-l', '--local', help='The local location', default=os.path.join(os.environ['HOME'], 'media'))
 parser.add_argument('dir', type=str, nargs='*', help='the directories to copy', default=home_dirs + storage_dirs)
 args = parser.parse_args()
-
 
 def sync(params, dirs, server_dir):
     if (args.backup):
@@ -40,11 +41,16 @@ def sync(params, dirs, server_dir):
     if (args.dry_run):
         params += 'n'
 
+    print(dirs)
     dirs = map(lambda x: os.path.join(src, x), dirs)
+
+    print(dirs)
 
     command = ['rsync', params, '--delete', '--filter=: .rsync']
     command.extend(dirs)
     command.append(dest)
+
+    print(command)
 
     run(command)
 
@@ -53,13 +59,15 @@ storage_dirs = [ x for x in args.dir if x in storage_dirs ]
 
 # rsync -a means:
 #   r: recursive
-#   l: copy symlinks
+#   l: when symlinks are encountered, recreate the symlink on the destination
 #   p: preserve permissions
 #   t: preserve times
 #   g: preserve group
 #   o: preserver owner
 #   D: equal to --devices --specials
-default_params = '-rltDv'
+#   k, --copy-dirlinks: causes the sending side to treat a symlink to a directory as though it were a real directory
+#   K, --keep-dirlinks
+default_params = '-rltDvkK'
 
 if (len(home_dirs) > 0):
     sync(default_params, home_dirs, home_root)
