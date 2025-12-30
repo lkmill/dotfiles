d06d7ef53dbaeae706a77f3763c4a61c1dd9f546
Linus Miller <linus.miller@bitmill.io>
2025-12-30 17:51:23 +0100

    nvim: use ranger instead of yazi

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -16,31 +16,21 @@ return {
     dependencies = {'nvim-lua/plenary.nvim'},
   },
   {
-  'mikavilpas/yazi.nvim',
-    event = 'VeryLazy',
-    keys = {
-      {
-        '<leader>r',
-        '<cmd>Yazi<cr>',
-        desc = 'Open yazi at the current file',
-      },
-      {
-        '<leader>R',
-        '<cmd>Yazi cwd<cr>',
-        desc = 'Open the file manager in nvim\'s working directory' ,
-      },
-      {
-        '<c-up>',
-        '<cmd>Yazi toggle<cr>',
-        desc = 'Resume the last yazi session',
-      },
-    },
-    opts = {
-      -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = false,
-      keymaps = {
-        show_help = '<f1>',
-      },
-    },
+    'kelly-lin/ranger.nvim',
+    config = function()
+      require('ranger-nvim').setup({
+        replace_netrw = true,
+        ui = {
+          width = 0.8,
+          height = 0.8,
+        },
+      })
+      vim.api.nvim_set_keymap('n', '<leader>r', "", {
+        noremap = true,
+        callback = function()
+          require('ranger-nvim').open(true)
+        end,
+      })
+    end,
   },
 }
