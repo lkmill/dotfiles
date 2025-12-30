a3636f4f2fd0695b614605804283d79b2df644b7
Linus Miller <linus.miller@bitmill.io>
2025-12-30 17:51:44 +0100

    nvim: use conform.nvim for auto-format

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -33,4 +33,18 @@ return {
       })
     end,
   },
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      format_on_save = {
+        -- I recommend these options. See :help conform.format for details.
+        lsp_format = "fallback",
+        timeout_ms = 500,
+      },
+    },
+  }
 }
